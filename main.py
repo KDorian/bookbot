40ddcc41f3fe1ee5f4f5e9111ed4bee96d029bcc
KDorian <dorian.kucharski@gamil.com>
2024-06-10 21:48:54 +0200

    Added character count

--- main.py
+++ main.py
@@ -5,11 +5,24 @@ def main():
     words = text.split()
     word_count = len(words)
     print(word_count)
+    # characters_count(text)
+    print(characters_count(text.lower()))
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
+def characters_count(s):
+    char_count = {}
+    
+    for char in s:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    
+    return char_count
 
 main()
